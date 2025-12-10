1f21c9d8b4f685244a036d2a19d35b9c32515a72
PH4N70M <yugrvaniya@gmail.com>
2025-12-10 13:33:50 +0530

    Added information about operations.

--- src/com/jasm/Instruction.java
+++ src/com/jasm/Instruction.java
@@ -1,46 +1,86 @@
 package com.jasm;
 
 public class Instruction {
-    public static final int NOP = 0;
+    // ==== NO OPERATION ====
+    public static final int NOP = 0; // Does no operation.
 
-    public static final int HLT = 1;
+    // ==== EXIT PROGRAM ====
+    public static final int HLT = 1; // Exits program.
 
-    public static final int LDI = 2;
-    public static final int ADD = 3;
-    public static final int SUB = 4;
-    public static final int MUL = 5;
-    public static final int AND = 6;
-    public static final int OR = 7;
-    public static final int XOR = 8;
-    public static final int ADI = 9;
-    public static final int SUI = 10;
+    // ==== BASIC INTEGER OPERATIONS ====
+    public static final int LDI = 2; // Loads a value into a register.
 
-    public static final int CAL = 11;
-    public static final int RET = 12;
+    public static final int ADD = 3; // Adds value of two registers and stores result in a third register.
 
-    public static final int LOD = 13;
-    public static final int STR = 14;
+    public static final int SUB = 4; // Subtracts value of two registers and stores result in a third register.
 
-    public static final int BRT = 15;
-    public static final int BRF = 16;
-    public static final int JMP = 17;
+    public static final int MUL = 5; // Multiplies value of two registers and stores result in a third register.
 
-    public static final int PRT = 18;
-    public static final int PRS = 19;
-    public static final int PRP = 20;
+    public static final int AND = 6; // Performs AND on the value of two registers and stores result in a third
+                                     // register.
 
-    public static final int CRR = 21;
-    public static final int CME = 22;
+    public static final int OR = 7; // Performs OR on the value of two registers and stores result in a third
+                                    // register.
 
-    public static final int LT = 23;
-    public static final int LTE = 24;
-    public static final int GT = 25;
-    public static final int GTE = 26;
-    public static final int EQU = 27;
-    public static final int NEQ = 28;
+    public static final int XOR = 8; // Performs XOR on the value of two registers and stores result in a third
+                                     // register.
 
-    public static final int INV = 29;
-    public static final int IRT = 30;
+    public static final int ADI = 9; // Adds a value to a register.
+
+    public static final int SUI = 10; // Subtracts a value from a register.
+
+    // ==== OPERATIONS RELATED TO FUNCTIONS ====
+    public static final int CAL = 11; // Used to call a function.
+
+    public static final int RET = 12; // Used to return from a function.
+
+    // ==== OPERATIONS RELATED TO MEMORY ====
+    public static final int LOD = 13; // Load a value from memory to register.
+
+    public static final int STR = 14; // Store value from register to memory.
+
+    // ==== BRANCHING OPERATIONS ====
+    public static final int BRT = 15; // Jump to index if value of register is not zero.
+
+    public static final int BRF = 16; // Jump to index if value of register is zero.
+
+    public static final int JMP = 17; // Jump to index.
+
+    // ==== PRINTING OPERATIONS ====
+    public static final int PRT = 18; // Print the value of a register.
+
+    public static final int PRS = 19; // Print the value of a registeras a string. ( %rx0 = 65, PRS %rx0 ==> A)
+
+    public static final int PRP = 20; // Print a String from the String pool.
+
+    // ==== OPERATIONS RELATED TO MEMORY ====
+    public static final int CRR = 21; // Clear memory.
+
+    public static final int CME = 22; // Clear registers.
+
+    // ==== BOOLEAN OPERATIONS ====
+    public static final int LT = 23; // Performs less than on the value of two registers and stores result in a third
+                                     // register.
+
+    public static final int LTE = 24; // Performs less than or equal to on the value of two registers and stores
+                                      // result in a third register.
+
+    public static final int GT = 25; // Performs greater than on the value of two registers and stores result in a
+                                     // third register.
+
+    public static final int GTE = 26; // Performs greater than or equal to on the value of two registers and stores
+                                      // result in a third register.
+
+    public static final int EQU = 27; // Performs equal to on the value of two registers and stores result in a third
+                                      // register.
+
+    public static final int NEQ = 28; // Performs not equal to on the value of two registers and stores result in a
+                                      // third register.
+
+    // ==== OPERATIONS RELATED TO EXTERNAL FUNCTIONS ====
+    public static final int INV = 29; // Invoke external function.
+
+    public static final int IRT = 30; // Return from external function.
 
     private static final Data data[] = {
             getDataFor("NOP", 0),
@@ -73,7 +113,7 @@ public class Instruction {
 
             getDataFor("CRR", 0),
             getDataFor("CME", 0),
-            
+
             getDataFor("LT", 3),
             getDataFor("LTE", 3),
             getDataFor("GT", 3),
